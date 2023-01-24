427703fb631893aaad136ae76b6d93feb0f8be01
valeriasouza28 <valeriasouza28@gmail.com>
2023-01-23 22:32:56 -0300

    update web

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,55 +1,79 @@
-import { HabitDay } from "./HabitDay"
-import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
+import { HabitDay } from "./HabitDay";
 
 const weekDays = [
-  "D", 
-  "S", 
-  "T", 
-  "Q", 
-  "Q", 
-  "S", 
-  "S"
-]
+  'D',
+  'S',
+  'T',
+  'Q',
+  'Q',
+  'S',
+  'S',
+];
 
 const summaryDates = generateDatesFromYearBeginning()
 
-// minimo dequadradinhos  que eu quero em tela que será 18 semanas vezes 7 
-const minimumSummaryDatesSize = 18 * 7
-// o quanto de quadradinhos que eupreciso mostar em tela para preencher a minha tabela de dias 
+const minimumSummaryDatesSize = 18 * 7 // 18 weeks
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
+type Summary = {
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}[]
+
 export function SummaryTable() {
-return (
-  <div className="w-full flex">
 
-    {/* dias da semana */}
-    <div className="grid grid-rows-7 grid-flow-row gap-3">
-      {/* coloca dia da semana domingo// text-xl texto de 20px,  */}
-      {weekDays.map((weekDays, i) => {
-        return (
-          <div key={`${weekDays}-${i}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center ">
-        {weekDays}
+  const [summary, setSummary] = useState<Summary>([])
+
+  useEffect(() =>{
+    api.get('summary').then(response => {
+      setSummary(response.data)
+    })
+  },[])
+
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, i) => {
+          return (
+            <div
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+            >
+              {weekDay}
+            </div>
+          )
+        })}
       </div>
-        )
-        })
-      }
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summary.length &&summaryDates.map((date) => {
+
+          const dayInSummary = summary.find(day => {
+            return dayjs(date).isSame(day.date, 'day')
+          })
+
+          return (
+            <HabitDay 
+              key={date.toString()}
+              date={date}
+              amount={dayInSummary?.amount} 
+              defaultCompleted={dayInSummary?.completed} 
+            />
+          )
+        })}
+
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+          return (
+            <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+          )
+        })}
+      </div>
     </div>
-
-      {/* Quadradinho do dia dos hábitos */}
-    <div className="grid grid-rows-7 grid-flow-col gap-3">
-      {/* para cada data percorrida ele retorna um componete HabitDay */}
-      {summaryDates.map(date => {
-        return <HabitDay key={date.toString()}/>
-      })}
-
-      {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, i) => {
-        return (
-          // cursor-not allowed é para deixar o quadradinho sem iteração, o icone do cursor do mouse muda para icone de block, como não temos um paramentro em map vamos passar o indice para em key
-          <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"/>
-      )
-      })}
-    </div>
-
-  </div>
-)
+  );
 }
